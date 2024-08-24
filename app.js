ec154d017f99da576137fa488ac11176bf050e07
Rawad Alharastani <ralharas@uwo.ca>
2024-08-24 17:48:15 -0400

    Finalize to deploy

--- app.js
+++ app.js
@@ -28,36 +28,6 @@ app.get('/contact', (req, res) => {
     res.render('contact');
 });
 
-app.post('/send', (req, res) => {
-    const { name, email, subject, message } = req.body;
-
-   
-    const transporter = nodemailer.createTransport({
-        service: 'Gmail', 
-        auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS,
-        }
-    });
-
-    const mailOptions = {
-        from: email,
-        to: 'rawadalh233@gmail.com',
-        subject: subject,
-        text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`
-    };
-
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            console.log(error);
-            res.redirect('/contact'); 
-        } else {
-            console.log('Email sent: ' + info.response);
-            res.render('index', { message: 'Your message has been sent!' }); 
-        }
-    });
-});
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
